ca959d46abdf2e8f1641bad4a78c9e1560ce6ef4
Anton Pashchenko <tony.pashchenko@gmail.com>
2016-12-08 00:22:07 +0200

    added temp & hum

--- src/node/node.ino
+++ src/node/node.ino
@@ -1,3 +1,7 @@
+#include "DHT.h"
+#include "DallasTemperature.h"
+#include "OneWire.h"
+
 #define SERIAL_BAUDRATE 115200
 
 #define OPC_PIN_MODE             0x01
@@ -7,6 +11,9 @@
 #define OPC_ANALOG_READ          0x05
 #define OPC_ANALOG_WRITE         0x06
 #define OPC_ANALOG_READ_AVERAGE  0x07
+#define OPC_PIN_MODE_DEFAULT     0x08
+#define OPC_READ_TEMP            0x09
+#define OPC_READ_TEMP_HUM        0x0A
 
 long pinVal = 0;
 long inpVal = 0;
@@ -14,19 +21,28 @@ long outVal = 0;
 
 void setup() {
     Serial.begin(SERIAL_BAUDRATE);
+
 }
 
 void loop() {
     pinVal = 0, inpVal = 0, outVal = 0;
     while (Serial.available() > 0) {
-      Serial.println('-');
     delay(1);
         switch (Serial.read()) {
             case OPC_PIN_MODE: {
                 delay(1);
                 pinVal = Serial.read();
+                inpVal = Serial.read();
+                pinMode(pinVal, inpVal);
+                break;
+            }
+            case OPC_PIN_MODE_DEFAULT: {
+                delay(1);
+                pinVal = Serial.read();
+                inpVal = Serial.read();
                 outVal = Serial.read();
-                pinMode(pinVal, outVal);
+                pinMode(pinVal, inpVal);
+                digitalWrite(pinVal, outVal);
                 break;
             }
             case OPC_DIGITAL_READ: {
@@ -89,6 +105,45 @@ void loop() {
                 analogWrite(inpVal, outVal);
                 break;
             }
+            case OPC_READ_TEMP: {
+                delay(1);
+                pinVal = Serial.read();
+                int num = Serial.read();
+                OneWire oneWire(pinVal);
+                DallasTemperature sensors(&oneWire);
+                sensors.begin();
+                sensors.requestTemperatures();
+                Serial.print(pinVal);
+                Serial.print('/');
+                for(int i = 0; i < num; i++) {
+                  Serial.print(sensors.getTempCByIndex(i)); 
+                  if (i + 1 < num) {
+                    Serial.print('/');
+                  }
+                }
+                Serial.println();
+                break;
+            }
+            case OPC_READ_TEMP_HUM: {
+                delay(1);
+                pinVal = Serial.read();
+                DHT dht(pinVal, DHT22);
+                dht.begin();
+                float h = dht.readHumidity();
+                float t = dht.readTemperature();
+                if (!isnan(h) && !isnan(t)) {
+                  float hic = dht.computeHeatIndex(t, h, false);
+                  Serial.print(pinVal);
+                  Serial.print('/');
+                  Serial.print(h);
+                  Serial.print('/');
+                  Serial.print(t);
+                  Serial.print('/');
+                  Serial.println(hic);
+                }
+                break;
+            }
         }
     }
 }
+
